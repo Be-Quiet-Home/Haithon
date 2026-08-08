61dd486e7960ef26f0d1e8381534b99d0154569d
Andreas Just <haiku-os@quietcode.org>
2026-08-08 07:05:22 +0200

    Preserve layout context listener lifetime

--- bindings/interface/LayoutContext.cpp
+++ bindings/interface/LayoutContext.cpp
@@ -25,10 +25,55 @@ py::class_<BLayoutContextListener,PyBLayoutContextListener>(m, "BLayoutContextLi
 .def("LayoutContextLeft", &BLayoutContextListener::LayoutContextLeft, "", py::arg("context"))
 ;
 
-py::class_<BLayoutContext>(m, "BLayoutContext")
+py::class_<BLayoutContext>(m, "BLayoutContext", py::dynamic_attr())
 .def(py::init(), "")
-.def("AddListener", &BLayoutContext::AddListener, "", py::arg("listener"))
-.def("RemoveListener", &BLayoutContext::RemoveListener, "", py::arg("listener"))
+.def("AddListener", [](BLayoutContext& self,
+        BLayoutContextListener* listener) {
+    self.AddListener(listener);
+    if (listener == nullptr)
+        return;
+
+    py::object owner
+        = py::cast(&self, py::return_value_policy::reference);
+
+    py::list listeners;
+    if (py::hasattr(owner, "_haithon_layout_context_listener_refs")) {
+        listeners = owner.attr(
+            "_haithon_layout_context_listener_refs").cast<py::list>();
+    } else {
+        listeners = py::list();
+        owner.attr("_haithon_layout_context_listener_refs") = listeners;
+    }
+
+    listeners.append(
+        py::cast(listener, py::return_value_policy::reference));
+}, "", py::arg("listener"))
+.def("RemoveListener", [](BLayoutContext& self,
+        BLayoutContextListener* listener) {
+    self.RemoveListener(listener);
+    if (listener == nullptr)
+        return;
+
+    py::object owner
+        = py::cast(&self, py::return_value_policy::reference);
+
+    if (!py::hasattr(owner, "_haithon_layout_context_listener_refs"))
+        return;
+
+    py::list listeners = owner.attr(
+        "_haithon_layout_context_listener_refs").cast<py::list>();
+
+    py::object target
+        = py::cast(listener, py::return_value_policy::reference);
+
+    for (py::ssize_t i = 0; i < py::len(listeners); i++) {
+        py::object item = listeners[i].cast<py::object>();
+        if (item.is(target)) {
+            listeners.attr("pop")(i);
+            break;
+        }
+    }
+}, "", py::arg("listener"))
 ;
 
 

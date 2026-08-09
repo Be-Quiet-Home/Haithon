fae899282669ec32ed552b3c389c655ac084126f
Andreas Just <haiku-os@quietcode.org>
2026-08-09 15:17:39 +0200

    Contain invalid picture playback binding

--- bindings/interface/Picture.cpp
+++ bindings/interface/Picture.cpp
@@ -10,12 +10,6 @@
 
 namespace py = pybind11;
 
-int PlayWrapper(BPicture& picture, BMessage* callBackTable, int32 tableEntries, void* userData) {
-    // Chiamare la funzione Play originale
-    void* callBackTableVoidPtr = static_cast<void*>(callBackTable);
-    return picture.Play(&callBackTableVoidPtr, tableEntries, userData);
-}
-
 PYBIND11_MODULE(Picture,m)
 {
 py::class_<BPicture, BArchivable>(m, "BPicture")
@@ -25,8 +19,6 @@ py::class_<BPicture, BArchivable>(m, "BPicture")
 .def_static("Instantiate", &BPicture::Instantiate, "", py::arg("data"))
 .def("Archive", &BPicture::Archive, "", py::arg("data"), py::arg("deep")=true)
 .def("Perform", &BPicture::Perform, "", py::arg("code"), py::arg("arg"))
-//.def("Play", &BPicture::Play, "", py::arg("callBackTable"), py::arg("tableEntries"), py::arg("userData"))
-.def("Play", &PlayWrapper, "", py::arg("callBackTable"), py::arg("tableEntries"), py::arg("userData"))
 .def("Flatten", &BPicture::Flatten, "", py::arg("stream"))
 .def("Unflatten", &BPicture::Unflatten, "", py::arg("stream"))
 //.def_readwrite("Private", &BPicture::Private, "")
